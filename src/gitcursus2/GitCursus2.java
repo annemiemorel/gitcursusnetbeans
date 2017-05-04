cc56ea01a5de58c955d7ad2c2f9f3c90d5c43481
Annemie Morel <annemie.morel@gmail.com>
2017-05-04 11:55:28 +0200

    eerste commit

---
+++ src/gitcursus2/GitCursus2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitcursus2;
+
+/**
+ *
+ * @author Annemie
+ */
+public class GitCursus2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
