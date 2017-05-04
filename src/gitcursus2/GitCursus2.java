7e8f937909a6e0e53c891f9d3af2755413265b5c
Annemie Morel <annemie.morel@gmail.com>
2017-05-04 12:05:38 +0200

    bonjour

--- src/gitcursus2/GitCursus2.java
+++ src/gitcursus2/GitCursus2.java
@@ -16,7 +16,7 @@ public class GitCursus2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hallo");
+        System.out.println("Bonjour");
     }
     
 }
